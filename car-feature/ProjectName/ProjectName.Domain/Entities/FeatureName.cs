29f5cfdd88510a3a3b1dfd55efcd1b1ef55b1694
Chinthaka Bandara <chinthakapb@gmail.com>
2024-09-13 09:29:09 +0530

    car-feature updated

--- car-feature/ProjectName/ProjectName.Domain/Entities/FeatureName.cs
+++ car-feature/ProjectName/ProjectName.Domain/Entities/FeatureName.cs
@@ -7,26 +7,47 @@ namespace ProjectName.Domain.Entities;
 
 public sealed class FeatureName : BaseEntity, IAuditableEntity
 {
-    private FeatureName() : base(default)
-    {
-        // Parameterless constructor for EF Core
-    }
-
-    public FeatureName(long companyId) : base(companyId)
-    {
-    }
+    public string Name { get; set; }
     public DateTime Created { get; set; }
     public long CreatedBy { get; set; }
     public DateTime Modified { get; set; }
     public long ModifiedBy { get; set; }
 
-    public Result<FeatureName> Update()
+    protected FeatureName() : base(default)
     {
-        throw NotImplementedException();
+        // Parameterless constructor for EF Core
+    }
+
+    private FeatureName(long companyId, string name) : base(companyId)
+    {
+        Name = name;
+    }
+
+    public static Result<FeatureName> Create(long companyId, string name)
+    {
+        // if (String.IsNullOrEmpty(name))
+        //     return Result.Failure<FeatureName>(new Error("FeatureName.Create", "Name is required"));
+
+        // var record = new FeatureName(companyId, name);
+        return Result.Success<FeatureName>(record);
+    }
+
+    public Result<FeatureName> Update(string name)
+    {
+        // if (String.IsNullOrEmpty(name))
+        //     return Result.Failure<FeatureName>(new Error("FeatureName.Update", "Name is required"));
+
+        //Name = name;
+        return Result.Success<FeatureName>(this);
     }
 
     public Result<FeatureName> Delete()
     {
-        throw NotImplementedException();
+        // if (Status == (int)RecordStatus.Deleted)
+        // {
+        //     return Result.Failure<FeatureName>(new Error("FeatureName.Delete", "Region has already been deleted"));
+        // }
+        // Status = (int)RecordStatus.Deleted;
+        return Result.Success<FeatureName>(this);
     }
 }
